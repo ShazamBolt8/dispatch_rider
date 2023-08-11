ea9f834f46c1836d77bcbd916b8439ac96f4660c
ShazamBolt8 <ShazamBolt8@gmail.com>
2023-08-12 00:26:57 +0500

    Moved files
    While doing local development, the files were not working in D drive. Moving from D:/ to C:/ solved the cause

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//these are used to add new hooks
+const webhookName = document.getElementById('webhookName');
+const webhookUrl = document.getElementById('webhookUrl');
+const addWebhook = document.getElementById('addWebhook');
+
+//these are used to share webhooks
+const shareCurrentHook = document.getElementById('shareCurrentHook');
+const shareAllHook = document.getElementById('shareAllHook');
+
+
+
