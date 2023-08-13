cc4973010062808a1e263d8e8779affb20f9ce6b
ShazamBolt8 <ShazamBolt8@gmail.com>
2023-08-13 19:37:41 +0500

    Updated codebase from single quotes to double quotes

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,9 +1,9 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === 'currentTab') {
+  if (request.message === "currentTab") {
     chrome.tabs.query({ active: true }, ([currTab]) => {
       sendResponse(currTab);
     });
-  } else if (request.message === 'allTab') {
+  } else if (request.message === "allTab") {
     chrome.tabs.query({}, (allTab) => {
       sendResponse(allTab);
     });
